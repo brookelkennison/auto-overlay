f07d5dde4c67f4f22e913cfa691354754fb64057
Brooke Kennison <brookelkennison@gmail.com>
2020-02-11 16:55:21 -0500

    feat: added NavBar and InputFeatures to AutoInfo

--- app/components/AutoInfo.js
+++ app/components/AutoInfo.js
@@ -1,19 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+function NavBar () {
+    return (
+        <div>
+            <nav>
+                <h1>Auto Overlay</h1>
+            </nav>
+        </div>
+    )
+}
+
 class UploadPhoto extends React.Component {
     render() {
         return (
-            <form action="#">
-                <input type="file" name="pic" accept="image/*" />
-                <input type="submit" />
-            </form>
+            <div>
+                <h3>Upload Image</h3>
+                <form action="#">
+                    <input type="file" name="pic" accept="image/*" />
+                </form>
+            </div>
         )
     }
 }
 
-UploadPhoto.PropTypes = {
-    photo: PropTypes.string.isRequired
+UploadPhoto.propTypes = {
+    photo: PropTypes.string
+}
+
+class InputFeatures extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Add Features</h3>
+                <form>
+                    <p>Feature 1:</p>
+                    <input type='text' name='name' />
+                    <p>Feature 2:</p>
+                    <input type='text' name='name' />
+                    <p>Feature 3:</p>
+                    <input type='text' name='name' />
+                    <p>Feature 4:</p>
+                    <input type='text' name='name' />
+                </form>
+            </div>
+        )
+    }
 }
 
 export default class AutoInfo extends React.Component {
@@ -23,7 +55,11 @@ export default class AutoInfo extends React.Component {
     render() {
         return (
             <React.Fragment>
+                <NavBar />
+                <br />
                 <UploadPhoto />
+                <br />
+                <InputFeatures />
             </React.Fragment>
         )
     }
