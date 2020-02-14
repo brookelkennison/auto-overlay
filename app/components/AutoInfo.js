e0b4efa0679d28870868d47d4a425b62c5b77798
Brooke Kennison <brookelkennison@gmail.com>
2020-02-14 08:35:16 -0500

    updated onChange handler for inputs

--- app/components/AutoInfo.js
+++ app/components/AutoInfo.js
@@ -29,19 +29,42 @@ UploadPhoto.propTypes = {
 }
 
 class InputFeatures extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            feat1: '',
+            feat2: '',
+            feat3: '',
+            feat4: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event){
+        this.setState({
+            feat1: event.target.value,
+            feat2: event.target.value,
+            feat3: event.target.value,
+            feat4: event.target.value
+        })
+    }
+    // Need to move handleSubmit for generate button
+    handleSubmit(event){
+        event.preventDefault();
+    }
     render() {
         return (
             <div>
                 <h3>Add Features</h3>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <p>Feature 1:</p>
-                    <input type='text' name='name' />
+                    <input type='text' name='feat1' value={this.feat1} onChange={this.handleChange} />
                     <p>Feature 2:</p>
-                    <input type='text' name='name' />
+                    <input type='text' name='feat2' value={this.feat2} onChange={this.handleChange} />
                     <p>Feature 3:</p>
-                    <input type='text' name='name' />
+                    <input type='text' name='feat3' value={this.feat3} onChange={this.handleChange} />
                     <p>Feature 4:</p>
-                    <input type='text' name='name' />
+                    <input type='text' name='feat4' value={this.feat4} onChange={this.handleChange} />
                 </form>
             </div>
         )
